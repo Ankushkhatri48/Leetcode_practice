e39ce97b02f96a225407c9d0ee7944fcc94432b1
Ankushkhatri48 <148999501+Ankushkhatri48@users.noreply.github.com>
2026-07-21 10:48:35 +0530

    Time: 0 ms (100%), Space: 8.1 MB (16.95%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -2,10 +2,14 @@ class Solution {
 public:
 
     int memo(int n, vector<int>& dp){
-        if(n==0 || n==1) return n;
+        if(n==0 || n==1) return dp[n]=n;
         if(dp[n] != -1) return dp[n];
         int x = memo(n-1,dp);
-        int y= memo(n-2,dp);
+        int y;
+        if(dp[n-2] != -1) y = dp[n-2];
+        else{
+         y = memo(n-2,dp);
+        }
         return dp[n] = x+y;
     }
     int fib(int n) {
