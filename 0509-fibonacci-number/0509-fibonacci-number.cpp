5797b999efbd7903acc51502bc022da6942a6e87
Ankushkhatri48 <148999501+Ankushkhatri48@users.noreply.github.com>
2026-07-21 11:01:25 +0530

    Time: 0 ms (100%), Space: 8.2 MB (11.26%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,19 +1,30 @@
 class Solution {
 public:
 
-    int memo(int n, vector<int>& dp){
-        if(n==0 || n==1) return dp[n]=n;
-        if(dp[n] != -1) return dp[n];
-        int x = memo(n-1,dp);
-        int y;
-        if(dp[n-2] != -1) y = dp[n-2];
-        else{
-         y = memo(n-2,dp);
-        }
-        return dp[n] = x+y;
-    }
+    // int memo(int n, vector<int>& dp){
+    //     if(n==0 || n==1) return dp[n]=n;
+    //     if(dp[n] != -1) return dp[n];
+    //     int x;
+    //     if(dp[n-1] != -1) x = dp[n-1];
+    //     else{
+    //     x = memo(n-1,dp);
+    //     }
+    //     int y;
+    //     if(dp[n-2] != -1) y = dp[n-2];
+    //     else{
+    //      y = memo(n-2,dp);
+    //     }
+    //     return dp[n] = x+y;
+    // }
     int fib(int n) {
+        if(n==0 || n==1) return n;
         vector<int> dp(n+1,-1);
-        return memo(n,dp);
+        dp[0] = 0;
+        dp[1] =1;
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+        //return memo(n,dp);
     }
 };
